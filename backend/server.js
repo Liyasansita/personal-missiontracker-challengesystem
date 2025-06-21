6d703085b8434ca57d878129bfcd79489dd1cc4f
Liyasansita <133018104+Liyasansita@users.noreply.github.com>
2025-06-21 17:06:46 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/personal-tracker", {
 app.use("/api/users", userRoutes);
 app.use("/api/challenges", challengeRoutes);
 
-const PORT = 5000;
+const PORT =process.env.PORT|| 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
